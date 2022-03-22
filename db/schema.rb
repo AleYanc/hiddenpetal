0cc5e6cb243d321480e59e82bde73135aa5fecd3
AleYanc <aleyanc2000@gmail.com>
2022-03-22 16:51:20 -0300

    Add new columns to models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_25_124535) do
+ActiveRecord::Schema.define(version: 2022_03_22_194928) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -68,6 +68,8 @@ ActiveRecord::Schema.define(version: 2021_10_25_124535) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "description"
     t.string "image"
+    t.integer "category_id", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -81,4 +83,5 @@ ActiveRecord::Schema.define(version: 2021_10_25_124535) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "products", "categories"
 end
