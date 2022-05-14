c4ed21847e1c3aa7780004a329892a6291daf982
AleYanc <aleyanc2000@gmail.com>
2022-05-14 20:39:09 -0300

    add automatic close to categories menu when user clicks outside of the menu area

--- app/javascript/js/filters.js
+++ app/javascript/js/filters.js
@@ -9,6 +9,7 @@ document.addEventListener('turbolinks:load', () => {
   categoriesFilterTitleResponsive = document.querySelector('.categories-filter-title-responsive');
   filtersOptionsOffcanvas = document.querySelector('.filter-select-box-options-offcanvas');
   closeBtn = document.querySelector('.fa-circle-xmark');
+  let body = document.querySelector("body");
 
   categoriesFiltersTitle.addEventListener('click', () => {
     filtersOptions.classList.toggle('filter-open');
@@ -16,9 +17,28 @@ document.addEventListener('turbolinks:load', () => {
 
   categoriesFilterTitleResponsive.addEventListener('click', () => {
     filtersOptionsOffcanvas.classList.add('offcanvas-open');
+    body.classList.add('disable-scroll')
   })
 
   closeBtn.addEventListener('click', () => {
     filtersOptionsOffcanvas.classList.remove('offcanvas-open');
+    body.classList.remove('disable-scroll')
   })
+})
+
+document.addEventListener('click', e => {
+  if(!($('.categories-filter-title').length > 0)) {
+    return;
+  }
+  
+  filtersOptionsOffcanvas = document.querySelector('.filter-select-box-options-offcanvas');
+  closeBtn = document.querySelector('.fa-circle-xmark');
+  let body = document.querySelector("body");
+
+  if(document.getElementById('filter-select-box-options-offcanvas').contains(e.target) || document.getElementById('filter-select-box-title-responsive').contains(e.target))   {
+    // do nothing
+  } else {
+    filtersOptionsOffcanvas.classList.remove('offcanvas-open');
+    body.classList.remove('disable-scroll')
+  }
 })
